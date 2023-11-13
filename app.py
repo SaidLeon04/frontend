a8c0ae22bcbe6d13509d68b3d12f276f595e4451
SaidLeon04 <1722110300@utectulancingo.edu.mx>
2023-11-13 17:13:17 -0600

    actualizacion

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     message = "Hello, World"
-    return render_template('index.html', message=message)
+    return render_template('buscar.html', message=message)
 
 @app.route("/buscar",methods=["GET","POST"])
 def buscar():
