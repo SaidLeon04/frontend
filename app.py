510c2d76f5be517bc3fd829bb14f0507f74c8ebe
SaidLeon04 <1722110300@utectulancingo.edu.mx>
2023-11-13 17:32:46 -0600

    actualizacion

--- app.py
+++ app.py
@@ -12,5 +12,21 @@ def index():
 def buscar():
     return render_template('buscar.html')
 
+@app.route("/borrar.html",methods=["GET", "DELETE"])
+def borrar():
+    return render_template('borrar.html')
+
+@app.route("/editar.html",methods=["GET", "PUT"])
+def editar():
+    return render_template('editar.html')
+
+@app.route("/insertar.html",methods=["GET", "POST"])
+def insertar():
+    return render_template('insertar.html')
+
+@app.route("/ver.html", methods=["GET"])
+def ver():
+    return render_template('ver.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
