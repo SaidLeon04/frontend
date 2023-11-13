853fd650969e96412c0548aaa8934373e69baee2
SaidLeon04 <1722110300@utectulancingo.edu.mx>
2023-11-13 17:48:38 -0600

    actualizacion

--- static/js/insert.js
+++ static/js/insert.js
@@ -20,7 +20,7 @@ function insert(email, nombre, telefono){
             console.log("response: " + response);
             console.log("json: " + json);
             console.log("status_code: " + request.status);
-            window.location.href = "../templates/index.html";
+            window.location.href = "https://frontentapi-de4686146bd2.herokuapp.com/";
         }
     }
     
